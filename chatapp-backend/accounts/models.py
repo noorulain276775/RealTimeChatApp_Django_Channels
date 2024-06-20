2d789754286691a909224ba8d821412c49e998b6
Noor ul ain Ibrahim <noor.ibrahim@softversum.com>
2024-06-20 15:18:55 +0400

    Started Django Channels

--- chatapp-backend/accounts/models.py
+++ chatapp-backend/accounts/models.py
@@ -31,12 +31,5 @@ class MyUser(AbstractUser, PermissionsMixin):
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.phone
+        return self.email
 
-
-# class OTP(models.Model):
-#     user_id = models.OneToOneField(MyUser, on_delete=models.CASCADE)
-#     otp = models.CharField(max_length=6)
-
-#     def __str__(self):
-#         return self.user_id
